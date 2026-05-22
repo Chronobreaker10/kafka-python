4423063c27a05a3740819ee462c803fc7bf6d99c
Phantom <nikita.dankin10@gmail.com>
2026-05-22 19:38:20 +0300

    Один продюсер, один консьюмер

---
+++ producer.py
@@ -0,0 +1,25 @@
+import asyncio
+
+from aiokafka import AIOKafkaProducer
+
+
+async def send():
+    producer = AIOKafkaProducer(bootstrap_servers='localhost:9092')
+    topic = 'test'
+    message = "Hello, Kafka!"
+    await producer.start()
+    i = 0
+
+    try:
+        while True:
+            msg_bytes = f'{message} #{i}'.encode('utf-8')
+            await producer.send_and_wait(topic, msg_bytes)
+            i += 1
+            print(f'Sent message {i}')
+            await asyncio.sleep(1)
+    finally:
+        await producer.stop()
+
+
+if __name__ == '__main__':
+    asyncio.run(send())
